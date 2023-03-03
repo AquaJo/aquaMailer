a3faf1c7c757ef431419a939bb1cda8d356dd979
AquaJo <84229101+AquaJo@users.noreply.github.com>
2023-03-03 16:21:49 +0100

    variable to string

--- config.js
+++ config.js
@@ -40,18 +40,18 @@ module.exports = {
                     backgroundTopicsAndPossibilities: [
                         ['https://source.unsplash.com/random/?futuristic', 45],
                         ['https://source.unsplash.com/random/?nature', 26],
-                        [gifArray, 24],
-                        [imgArray, 12]
+                        [['https://www.site1.com/gif1','https://www.site2.com/gif2'], 24],
+                        [['https://www.site1.com/img1','https://www.site2.com/img2'], 12]
                     ], // set random themes || image/ gif links with specific possibilites for mail cover
 
                     // some HTML-Manipulation-Options from top to bottom
                     ejs_Logo: "", // want to show a logo at the top left? set url here or leave "" or null for no logo
                     ejs_ImageTitle: 'server-forwarder', // sets the title of the heading placed inside the image cover
-                    ejs_Greeting: 'Hey AquaJo,', // sets greeting, placed direct under image
-                    ejs_Info: 'server sent us a new message from your homepage aquajo.me', // sets the info message under the greeting
+                    ejs_Greeting: 'Hey there,', // sets greeting, placed direct under image
+                    ejs_Info: 'server sent us a new message from your homepage', // sets the info message under the greeting
 
                     buttonLeft: ["replit.com", "https://replit.com/~"], // set button text and button href || both null
-                    buttonRight: ["aquajo.me", "https://aquajo.me"] // same for the right button
+                    buttonRight: [null, null] // same for the right button
                 },
 
                 bumping: {
