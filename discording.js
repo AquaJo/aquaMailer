1c2bf59764e115661deb2b2aa0057448eeb8c3f7
AquaJo <84229101+AquaJo@users.noreply.github.com>
2024-01-12 17:13:33 +0100

    added slash-command-support

--- discording.js
+++ discording.js
@@ -1,4 +1,5 @@
-const { Client, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, REST } = require('discord.js');
+const { Routes } = require('discord-api-types/v9');
 const configFirst = require("./config.js");
 const config = configFirst.config;
 
@@ -9,44 +10,82 @@ const client = new Client({
     GatewayIntentBits.GuildMembers,
     GatewayIntentBits.MessageContent,
   ],
-})
+});
 
+const commands = [
+  {
+    name: "ping",
+    description: "Test the bot's responsiveness with a simple ping command",
+  },
+  {
+    name: "version",
+    description: "Get the current version of the bot",
+  },
+  {
+    name: "dm",
+    description: "Send a direct message to the user",
+  },
+  // Add more commands as needed
+];
 
-client.on('ready', async () => {
-  console.log("bot is ready");
+const rest = new REST({ version: '9' }).setToken(config.discord.config.dcToken);
 
-  const Guilds = client.guilds.cache.map(guild => guild.id);
-  const guild = client.guilds.cache.get(Guilds[0]);  // (...)
-  const members = await guild.members.fetch();
-})
+client.once('ready', async () => {
+  console.log("Bot is ready");
+
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationCommands(client.user.id),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 client.on('messageCreate', message => {
   if (message.content === "!ping") message.reply('pong');
-  if (message.content === "!version") message.reply("0.2")
-  if (message.content === ("!dm")) {
+  if (message.content === "!version") message.reply("0.2");
+  if (message.content === "!dm") {
     client.users.cache.get(message.author.id).send('hi');
   }
-})
+});
+
+// Discord.js Slash Commands
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'ping') {
+    await interaction.reply('Pong!');
+  } else if (commandName === 'version') {
+    await interaction.reply('0.2');
+  } else if (commandName === 'dm') {
+    await interaction.user.send('Hi!');
+    await interaction.reply('Message sent!');
+  }
+});
 
 client.on("warn", () => {
-  console.log("warn")
-})
-
+  console.log("Warn");
+});
 
 client.on('rateLimit', (rateLimitInfo) => {
   console.log(rateLimitInfo);
 });
 
-function sendMsg(ids, msg) { 
+function sendMsg(ids, msg) {
   for (let i = 0; i < ids.length; ++i) {
-    const user = client.users.cache.find(u => u.tag === ids[i])
+    const user = client.users.cache.find(u => u.tag === ids[i]);
     user.send(msg);
   }
 }
 
-
 client.login(config.discord.config.dcToken);
-
-
 
 module.exports = { sendMsg };
